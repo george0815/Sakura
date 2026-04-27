de94e55c0fa286118dbc66fc868324c91839cdb5
George S. <shipmang8@gmail.com>
2026-04-27 15:53:01 +0900

    Finished instruction scaffolding

--- CORE/CPU_6502/opcodes.cpp
+++ CORE/CPU_6502/opcodes.cpp
@@ -7,8 +7,19 @@ namespace {
 
 }
 
+//Builds lookup table, which is basically an vector of INSTRUCTION structs using the opcode as the index
+void CPU_6502::BUILD_LOOKUP(){
 
-CPU_6502::
+
+
+
+
+}
+
+
+
+
+
 
 //Helper function for branching TODO
 void CPU_6502::BRANCH(){
@@ -255,7 +266,7 @@ void CPU_6502::CPY(uint16_t addr){
 
 
 
-
+//Decrement memory, subtracts 1 from a memory location
 void CPU_6502::DEC(uint16_t addr){
 
 
@@ -267,7 +278,7 @@ void CPU_6502::DEC(uint16_t addr){
 
 
 
-
+//Decrement X, subtracts 1 from the X register 
 void CPU_6502::DEX(uint16_t addr){
 
 
@@ -279,7 +290,7 @@ void CPU_6502::DEX(uint16_t addr){
 
 
 
-
+//Decrement Y, subtracts 1 from the Y register
 void CPU_6502::DEY(uint16_t addr){
 
 
@@ -291,7 +302,7 @@ void CPU_6502::DEY(uint16_t addr){
 
 
 
-
+//Bitwise exclusive OR, XORs a memory value with the accumulator, bit by bit
 void CPU_6502::EOR(uint16_t addr){
 
 
@@ -304,7 +315,7 @@ void CPU_6502::EOR(uint16_t addr){
 
 
 
-
+//Increment memory, 1 is added to a memory value
 void CPU_6502::INC(uint16_t addr){
 
 
@@ -317,6 +328,7 @@ void CPU_6502::INC(uint16_t addr){
 
 
 
+//Increment Y, 1 is added to the Y register
 void CPU_6502::INY(uint16_t addr){
 
 
@@ -327,8 +339,7 @@ void CPU_6502::INY(uint16_t addr){
 
 
 
-
-
+//Increment X, 1 is added to the X register
 void CPU_6502::INX(uint16_t addr){
 
 
@@ -341,7 +352,8 @@ void CPU_6502::INX(uint16_t addr){
 
 
 
-
+//Jump, sets the PC to a new value, allowing code to execute from a new location 
+//JMP BUG: if the new 2 byte PC crosses a page (ends in FF), then the CPU fails to increment the page wheen reading the second byte
 void CPU_6502::JMP(uint16_t addr){
 
 
@@ -353,7 +365,9 @@ void CPU_6502::JMP(uint16_t addr){
 
 
 
-
+//Pushes the current program counter to the stack, then sets the PC to a new value
+//then the code can call a function and return using RTS
+//the return address on the stack points 1 byte before the start of the next instruction
 void CPU_6502::JSR(uint16_t addr){
 
 
@@ -366,7 +380,7 @@ void CPU_6502::JSR(uint16_t addr){
 
 
 
-
+//Loads a memory value into the accumulator
 void CPU_6502::LDA(uint16_t addr){
 
 
@@ -378,6 +392,7 @@ void CPU_6502::LDA(uint16_t addr){
 
 
 
+//Loads a memory value into the X register
 void CPU_6502::LDX(uint16_t addr){
 
 
@@ -388,8 +403,7 @@ void CPU_6502::LDX(uint16_t addr){
 
 
 
-
-
+//Loads a memory value into the Y register
 void CPU_6502::LDY(uint16_t addr){
 
 
@@ -401,7 +415,9 @@ void CPU_6502::LDY(uint16_t addr){
 
 
 
-
+//Logical shift right, shifts all bits of a memory value or the accumulator one position to the right
+//0 is shifted into bit 7, and 0 is shifted to the carry flag
+//equivalent of dividing an unsigned integer by 2 and rounding down, with the remainder in the carry flag
 void CPU_6502::LSR(uint16_t addr){
 
 
@@ -413,19 +429,12 @@ void CPU_6502::LSR(uint16_t addr){
 
 
 
-
-void CPU_6502::NOP(uint16_t addr){
-
-
+//No operation, literally just does fucking nothing and wastes space and CPU cycles 
+void CPU_6502::NOP(uint16_t addr){}
 
 
 
-}
-
-
-
-
-
+//Bitwise OR, ORs a memory value and the accumulator, bit by bit
 void CPU_6502::ORA(uint16_t addr){
 
 
@@ -438,7 +447,8 @@ void CPU_6502::ORA(uint16_t addr){
 
 
 
-
+//Push A, stores the value of the accumulator to the current stack position
+//then decrements the stack pointer 
 void CPU_6502::PHA(uint16_t addr){
 
 
@@ -449,7 +459,8 @@ void CPU_6502::PHA(uint16_t addr){
 
 
 
-
+//Push processor status, pushes a byte representing the status register to the stack, then decrements the stack pointer 
+//the B flag and the extra bit are both pushed as one
 void CPU_6502::PHP(uint16_t addr){
 
 
@@ -461,7 +472,7 @@ void CPU_6502::PHP(uint16_t addr){
 
 
 
-
+//Pull A, increments the stack pointer then loads that value at that stack position into A
 void CPU_6502::PLA(uint16_t addr){
 
 
@@ -473,6 +484,7 @@ void CPU_6502::PLA(uint16_t addr){
 
 
 
+//Pull processor status, increments the stack pointer then loads that byte into the 6 flags
 void CPU_6502::PLP(uint16_t addr){
 
 
@@ -483,7 +495,9 @@ void CPU_6502::PLP(uint16_t addr){
 
 
 
-
+//Rotate left, shifts a memory value or the accumulator to the left
+//the carry flag is treated as though it is both above bit 7 and below bit 0
+//so bit 7 is shifted to carry, and carry is shifted to bit 0
 void CPU_6502::ROL(uint16_t addr){
 
 
@@ -496,6 +510,9 @@ void CPU_6502::ROL(uint16_t addr){
 
 
 
+//Rotate right, shifts a memory value or the accumulator to the right
+//the carry flag is treated as though it is both above bit 7 and below bit 0
+//so bit 7 is shifted to carry, and carry is shifted to bit 0
 void CPU_6502::ROR(uint16_t addr){
 
 
@@ -507,7 +524,8 @@ void CPU_6502::ROR(uint16_t addr){
 
 
 
-
+//Return from interrupt, returns from an interrupt handler by pulling the status flags from the stack, then pulling the new program counter
+//similar to PLP but changes to the interrupt disable flag apply immediately instead of being delayed one instruction
 void CPU_6502::RTI(uint16_t addr){
 
 
@@ -520,7 +538,7 @@ void CPU_6502::RTI(uint16_t addr){
 
 
 
-
+//Return from subroutine, pulls an address from the stack into the program counter, then increments the program counter
 void CPU_6502::RTS(uint16_t addr){
 
 
@@ -532,7 +550,10 @@ void CPU_6502::RTS(uint16_t addr){
 
 
 
-
+//Subtract with carry, subtracts a memory value and the NOT of the carry flag from the accumulator 
+//it does this by adding the bitwise NOT of the memory value using ADC
+//carry is cleared when it underflows and set otherwise
+//overflow works the same as ADC, except with an intverted memory value 
 void CPU_6502::SBC(uint16_t addr){
 
 
@@ -544,7 +565,7 @@ void CPU_6502::SBC(uint16_t addr){
 
 
 
-
+//Set carry, sets the carry flag
 void CPU_6502::SEC(uint16_t addr){
 
 
@@ -557,6 +578,7 @@ void CPU_6502::SEC(uint16_t addr){
 
 
 
+//Set decimal, sets the decimal flag
 void CPU_6502::SED(uint16_t addr){
 
 
@@ -569,6 +591,7 @@ void CPU_6502::SED(uint16_t addr){
 
 
 
+//Set interrupt disable, sets the interrupt disable flag
 void CPU_6502::SEI(uint16_t addr){
 
 
@@ -579,7 +602,7 @@ void CPU_6502::SEI(uint16_t addr){
 
 
 
-
+//Store accumulator, stores the accumulator value into memory
 void CPU_6502::STA(uint16_t addr){
 
 
@@ -591,6 +614,7 @@ void CPU_6502::STA(uint16_t addr){
 
 
 
+//Store X, stores the X register value into memory
 void CPU_6502::STX(uint16_t addr){
 
 
@@ -601,6 +625,7 @@ void CPU_6502::STX(uint16_t addr){
 
 
 
+//Store Y, stores the Y register value into memory
 void CPU_6502::STY(uint16_t addr){
 
 
@@ -611,7 +636,7 @@ void CPU_6502::STY(uint16_t addr){
 
 
 
-
+//Transfer A to X, copies the accumulator value to the X register
 void CPU_6502::TAX(uint16_t addr){
 
 
@@ -623,6 +648,7 @@ void CPU_6502::TAX(uint16_t addr){
 
 
 
+//Transfer A to Y, copies the accumulator value to the Y register
 void CPU_6502::TAY(uint16_t addr){
 
 
@@ -635,6 +661,7 @@ void CPU_6502::TAY(uint16_t addr){
 
 
 
+//Transfer stack pointer to X, copies the stack pointer value to the X register
 void CPU_6502::TSX(uint16_t addr){
 
 
@@ -646,6 +673,7 @@ void CPU_6502::TSX(uint16_t addr){
 
 
 
+//Transfer X to the accumulator, copies the X register value to the accumulator
 void CPU_6502::TXA(uint16_t addr){
 
 
@@ -658,6 +686,7 @@ void CPU_6502::TXA(uint16_t addr){
 
 
 
+//Transfer X to the stack pointer, copies the X register value to the stack pointer
 void CPU_6502::TXS(uint16_t addr){
 
 
@@ -668,6 +697,7 @@ void CPU_6502::TXS(uint16_t addr){
 
 
 
+//Transfer Y to the accumulator, copies the Y register value to the accumulator
 void CPU_6502::TYA(uint16_t addr){
 
 
@@ -679,12 +709,7 @@ void CPU_6502::TYA(uint16_t addr){
 //UNIMPLEMENTED/INVALID 
 
 
-void CPU_6502::XXX(uint16_t addr){
-
-
-
-
-}
+void CPU_6502::XXX(uint16_t addr){}
 
 
 
