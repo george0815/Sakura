021e399151f3c5dbee0f4599c1222c50cf17f114
George S. <shipmang8@gmail.com>
2026-04-27 13:31:14 +0900

    Added scaffolding for opcodes in header file

--- CORE/CPU_6502/core.h
+++ CORE/CPU_6502/core.h
@@ -69,4 +69,68 @@ public:
   uint16_t ABSOLUTE();
   uint16_t RELATIVE();
   uint16_t INDIRECT();
+
+
+
+  //OFFICIAL INSTRUCTIONS
+  void ADC(uint16_t addr);
+  void AND(uint16_t addr);
+  void ASL(uint16_t addr);
+  void BCC(uint16_t addr);
+  void BCS(uint16_t addr);
+  void BEQ(uint16_t addr);
+  void BIT(uint16_t addr);
+  void BMI(uint16_t addr);
+  void BNE(uint16_t addr);
+  void BPL(uint16_t addr);
+  void BRK(uint16_t addr);
+  void BVC(uint16_t addr);
+  void BVS(uint16_t addr);
+  void CLC(uint16_t addr);
+  void CLD(uint16_t addr);
+  void CLI(uint16_t addr);
+  void CLV(uint16_t addr);
+  void CMP(uint16_t addr);
+  void CPX(uint16_t addr);
+  void CPY(uint16_t addr);
+  void DEC(uint16_t addr);
+  void DEX(uint16_t addr);
+  void DEY(uint16_t addr);
+  void EOR(uint16_t addr);
+  void INC(uint16_t addr);
+  void INY(uint16_t addr);
+  void INX(uint16_t addr);
+  void JMP(uint16_t addr);
+  void JSR(uint16_t addr);
+  void LDA(uint16_t addr);
+  void LDX(uint16_t addr);
+  void LDY(uint16_t addr);
+  void LSR(uint16_t addr);
+  void NOP(uint16_t addr);
+  void ORA(uint16_t addr);
+  void PHA(uint16_t addr);
+  void PHP(uint16_t addr);
+  void PLA(uint16_t addr);
+  void PLP(uint16_t addr);
+  void ROL(uint16_t addr);
+  void ROR(uint16_t addr);
+  void RTI(uint16_t addr);
+  void RTS(uint16_t addr);
+  void SBC(uint16_t addr);
+  void SEC(uint16_t addr);
+  void SED(uint16_t addr);
+  void SEI(uint16_t addr);
+  void STA(uint16_t addr);
+  void STX(uint16_t addr);
+  void STY(uint16_t addr);
+  void TAX(uint16_t addr);
+  void TAY(uint16_t addr);
+  void TSX(uint16_t addr);
+  void TXA(uint16_t addr);
+  void TXS(uint16_t addr);
+  void TYA(uint16_t addr);
+
+
+
+
 };
